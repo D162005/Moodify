67483ceb9262b0f66f237a092c635a9dfc240fc0
Darshan Patil <darshanmpatil16012005@gmail.com>
2026-07-08 18:14:30 +0530

    deployment intigration 2

--- frontend/src/features/home/services/song.api.js
+++ frontend/src/features/home/services/song.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/song",
+    baseURL: "https://moodify-o0p9.onrender.com/api/song",
     withCredentials: true
 })
 
